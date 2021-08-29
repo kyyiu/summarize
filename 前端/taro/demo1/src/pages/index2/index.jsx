fe182c31eae310c14142459d5187bfeac942d9af
huanghao <1308936944@qq.com>
2021-08-29 19:24:59 +0800

    '更新git.txt'

--- 前端/taro/demo1/src/pages/index2/index.jsx
+++ 前端/taro/demo1/src/pages/index2/index.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Taro from '@tarojs/taro'
-import { View, Text, Button } from '@tarojs/components'
+import { View, Text, Button, ScrollView } from '@tarojs/components'
 import './index.less'
 
 export default function Index2() {
 	
 	const [ title2, setTitle2 ] = useState('title2');
+	
+	useEffect(()=>{
+		
+	})
 	
 	const go2Index = () => {
 		Taro.navigateTo({
@@ -13,17 +17,43 @@ export default function Index2() {
 		})
 	} 
 	
+	const sty = {
+		height: '100px',
+		background: 'red',
+	}
+	
+	const sonS = {
+		height: '300px',
+		background: 'rgba(0,0,0,.3)',
+	}
+	
 	const go2Index_2 = () => {
 		Taro.redirectTo({
 			url: '/pages/index/index'
 		})
 	} 
 	
+	let t = 0;
+	
 	return (
 		<View>
+			<View>
+				{
+					[1,2,3].map(it=>{
+						++t
+						console.log(t)
+						if(t<3){
+							return (<View>{t}</View>)
+						}
+					})
+				}
+			</View>
 			<Text>页面2</Text>
 			<Button onClick={go2Index}>去index页面</Button>
 			<Button onClick={go2Index_2}>去index页面,不能返回上一级页面</Button>
+			<ScrollView onScroll={e=>console.log(e)} style={sty} scrollY>
+				<View style={sonS}>342</View>
+			</ScrollView>
 		</View>
 	)
 	
